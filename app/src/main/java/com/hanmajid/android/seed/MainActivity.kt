21db58919102c485dbbc28441c8b72e6468335b0
hanmajid <han.majid2004@gmail.com>
2020-06-26 22:11:44 +0700

    Initial commit

---
+++ app/src/main/java/com/hanmajid/android/seed/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hanmajid.android.seed
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
